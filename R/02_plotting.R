62f3b8f25a74ae5fb23f41e9b109b14b9edfe4b2
Cole Brookson <cole.brookson@gmail.com>
2023-11-07 12:59:27 -0500

    taking out the model prediction stuff

--- R/02_plotting.R
+++ R/02_plotting.R
@@ -5,36 +5,10 @@
 # read in model object and dataframe ===========================================
 
 rich_df <- readr::read_csv(here::here("./data/clean/richness-data.csv"))
-mod <- readRDS(here::here("./outputs/best-model.rds"))
-
-# get model predictions ========================================================
-
-# make new data to regress against -- use expand.grid()
-full_df <- expand.grid(
-  biomass = seq(min(rich_df$biomass), max(rich_df$biomass), length.out = 200),
-  # note that pH has to be a factor here
-  ph = levels(as.factor(rich_df$ph)))
-
-# make model predictions
-mod_pred <- stats::predict(
-  mod,
-  newdata = full_df,
-  type = "response"
-)
-
-# bind to a dataframe
-predict_df <- data.frame(
-  cbind(full_df,
-        n_spp = mod_pred)
-)
-head(predict_df)
 
 # make results plot ============================================================
 
 plot <- ggplot2::ggplot() +
-  ggplot2::geom_line(data = predict_df, 
-                     ggplot2::aes(x = biomass, y = n_spp, colour = ph),
-                     linewidth = 1.3) +
   ggplot2::geom_point(data = rich_df, 
                       ggplot2::aes(x = biomass, y = n_spp, fill = ph),
                       shape = 21, size = 3) +
