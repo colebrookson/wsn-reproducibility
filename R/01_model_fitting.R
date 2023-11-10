0f2b80f3a9b5b1e462b643de8e4b29aa723deba0
Alex Davis <adavis1152@gmail.com>
2023-11-09 16:00:32 -0800

    removing as.factor

--- R/01_model_fitting.R
+++ R/01_model_fitting.R
@@ -8,8 +8,6 @@ rich_df <- readr::read_csv(here::here("./data/clean/richness-data.csv"),
 
 # model fitting ================================================================
 
-# make character variable a factor
-rich_df$ph <- as.factor(rich_df$ph)
 
 # compare between just biomass and biomass interacting with pH
 mod1 <- stats::glm(n_spp ~ biomass, data = rich_df,
